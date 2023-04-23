cd5ab940ea701cd1966517a25170f3f8a93f641a
Бушуев Роман <1997pom@gmail.com>
2023-04-24 09:11:14 +1000

    ALLOWED_HOSTS

--- djangoProject_back4app/settings.py
+++ djangoProject_back4app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^72ea5h=*1z7kf69-hz!p4c5(u=)30s34cobrobhn2%%z+aoas
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
